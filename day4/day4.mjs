c9df4725bb139fcc4064b471b1db86dfb32c0678
anselm <ansf@users.noreply.github.com>
2022-12-04 20:42:38 +0100

    Day4

---
+++ day4/day4.mjs
@@ -0,0 +1,18 @@
+import fs from "node:fs"
+
+const pairs = fs.readFileSync("input.txt", "utf-8")
+    .trim()
+    .split("\n")
+    .map(s => s.split(",").map(s => s.split("-").map(s => Number(s))))
+
+let count = 0
+
+for (const pair of pairs) {
+    if ((pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1])
+        || (pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1])) {
+        count++
+    }
+}
+
+
+console.log(count)
